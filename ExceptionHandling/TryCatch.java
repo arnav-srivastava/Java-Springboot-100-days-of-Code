95952dfa8bb8265a7d960d2c0aee7488fd35034c
arnav-srivastava <arnavsrivastav398@gmail.com>
2025-08-23 08:14:36 +0530

    Undertanding  Checked vs Unchecked exception

---
+++ ExceptionHandling/TryCatch.java
@@ -0,0 +1,30 @@
+package ExceptionHandling;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class TryCatch {
+    public static void main(String[] args) {
+
+        // try-catch-finally example
+        try{
+            int i=10/0;
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero "+e.getMessage());
+        }finally {
+            System.out.println("Finally execution completed");
+        }
+
+        //checked exception at compile time
+        try{
+            FileReader rd=new FileReader("file.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        }
+
+        //unchecked exception at runtime
+        String text = null;
+        System.out.println(text.length());
+
+    }
+}
